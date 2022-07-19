5120561f692824972cdf03fda975211eecfba5d7
HAN-JEONGSOO <104303320+HAN-JEONGSOO@users.noreply.github.com>
2022-07-19 09:27:45 +0900

    Update 5_QuadraticEquation.py

--- Step_1/5_QuadraticEquation.py
+++ Step_1/5_QuadraticEquation.py
@@ -1,16 +1,16 @@
-# Solve the quadratic equation ax**2 + bx + c = 0
+# 2차방정식 해 구하기 ax**2 + bx + c = 0
 
-# import complex math module
+# math module Import하기
 import cmath
 
 a = 1
 b = 5
 c = 6
 
-# calculate the discriminant
+# 루트 안에 들어갈 판별식 d
 d = (b**2) - (4*a*c)
 
-# find two solutions
+# 두 가지 +- 모두 정의
 sol1 = (-b-cmath.sqrt(d))/(2*a)
 sol2 = (-b+cmath.sqrt(d))/(2*a)
 
