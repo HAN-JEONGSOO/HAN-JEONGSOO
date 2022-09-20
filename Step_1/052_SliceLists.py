f439069a3e7782adbd91ec59af6486a0cf69b975
HAN-JEONGSOO <104303320+HAN-JEONGSOO@users.noreply.github.com>
2022-09-20 12:32:46 +0900

    Create 052_SliceLists.py

---
+++ Step_1/052_SliceLists.py
@@ -0,0 +1,54 @@
+# Get all the Items
+my_list = [1, 2, 3, 4, 5]
+
+print(my_list[:])
+
+# [1, 2, 3, 4, 5]
+
+
+
+# Get all the Items After a Specific Position
+my_list = [1, 2, 3, 4, 5]
+
+print(my_list[2:])
+
+# [3, 4, 5]
+
+
+
+# Get all the Items Before a Specific Position
+my_list = [1, 2, 3, 4, 5]
+
+print(my_list[:2])
+
+# [1, 2]
+
+
+
+# Get all the Items from One Position to Another Position
+my_list = [1, 2, 3, 4, 5]
+
+print(my_list[2:4])
+
+# [3, 4]
+
+
+
+# Get the Items at Specified Intervals
+my_list = [1, 2, 3, 4, 5]
+
+print(my_list[::2])
+
+# [1, 3, 5]
+
+my_list = [1, 2, 3, 4, 5]
+
+print(my_list[::-2])
+
+# [5, 3, 1]
+
+my_list = [1, 2, 3, 4, 5]
+
+print(my_list[1:4:2])
+
+# [2, 4]
